fa1bf8fa73098026b58551bf9f7d766a24b277d8
thelamer <ryankuba@gmail.com>
2025-05-29 15:15:56 -0400

    fix project naming

--- src/selkies_gstreamer/__main__.py
+++ src/selkies_gstreamer/__main__.py
@@ -1,12 +1,15 @@
 import argparse
-from websocket import ws_entrypoint
-from webrtc import wr_entrypoint
+from selkies_gstreamer.websocket import ws_entrypoint
+from selkies_gstreamer.webrtc import wr_entrypoint
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mode')
-    args, unknown = parser.parse_known_args()
+    parser.add_argument('--mode', help="Specify the mode: 'webrtc' or 'websocket'")
+    args = parser.parse_args()
     if args.mode == 'webrtc':
       wr_entrypoint()
     else:
       ws_entrypoint()
+
+if __name__ == "__main__":
+    main()
